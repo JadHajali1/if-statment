1e4abb732664afc0df4e4a3c57fe8200fcb39bcf
JadHajali1 <jadhajali773@gmail.com>
2025-02-13 22:28:04 +0200

    solution

--- ex1.py
+++ ex1.py
@@ -1,1 +1,19 @@
-print("hi")
+age = int(input("please enter your age: "))
+price = 0
+
+if age >= 40:
+    price = 25
+
+elif age >= 30:
+    price = 20
+
+elif age >= 20:
+    price = 15
+
+elif age >= 10:
+    price = 10
+
+else:
+    price = 5
+
+print(f"The ticket price is ${price}.")
